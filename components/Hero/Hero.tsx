0475301261f3c5c383b656fe9d2986f79eade35b
k0wal45 <d.kowal.com@gmail.com>
2024-04-16 23:29:35 +0200

    small ui update

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -19,21 +19,11 @@ function Hero() {
 return (
 		<section className="flex justify-center items-end min-h-screen max-w-screen relative overflow-hidden border-b-2 border-neutral-200 p-4 pb-0">
 			<div className="text-start flex flex-col lg:flex-row lg:pb-0 pt-32 lg:pt-0 justify-center items-center gap-24">
-				
 				<div className="flex flex-col gap-4 max-w-md">
-					
 						<h1 className="text-5xl font-[800] h-40"><TyperWriter sequence={heroText}/></h1>
-					
-					
 						<p className="text-xl" >W dzisiejszych czasach strona internetowa i efektywny logotyp stanowią kluczowe elementy marketingu, które mają ogromne znaczenie zarówno online, jak i w świecie biznesu poza siecią.</p>
-					
-					
 						<BtnLink link="/kontakt">Skontaktuj się po darmową wycenę</BtnLink> 
-					
-
 				</div>
-				
-
 				<div className={classes.circleContainer}>
 					<Image src='/img/beztla.png' alt="Daniel Kowalski Zdjęcie" width={1000} height={1000} className="max-h-[90vh] block z-30 object-contain"/>
 				</div>
