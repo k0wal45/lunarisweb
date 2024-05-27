3760fd8dfa5677087b1393ca781410059f11f595
k0wal45 <d.kowal.com@gmail.com>
2024-05-27 13:00:31 +0200

    tgwser

--- components/reviews/ReviewData.ts
+++ components/reviews/ReviewData.ts
@@ -1,41 +1,41 @@
 const data = [
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/1.png',
     name: 'Dariusz Kowalski',
     opinion: 'Zainwestowałem w tę stronę i nie żałuje! To nie tylko świetnie zaprojektowana witryna, ale też działa jak złoto. Bez zbędnego gadania, polecam ich każdemu, kto chce mocnej strony i obsługi na najwyższym poziomie!',
     rate: '5',
     link: 'https://g.co/kgs/PAQwA3h'
   },
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/2.png',
     name: 'Magdalena Krawczyk',
     opinion: 'Polecam firmę. Profesjonalne podejście do zlecenia, błyskawiczną reakcja na życzenie klienta. Rozsądne ceny usług. Na pewno zrealizujemy jeszcze nie jedno zlecenie.',
     rate: '5',
     link: 'https://g.co/kgs/55AVSkF'
   },
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/3.png',
     name: 'Dorota Domańska',
     opinion: 'Polecam super kontakt, indywidualne podejście, szybka realizacja. Jestem bardzo zadowolona ze strony internetowej - uzyskałam efekt, którego oczekiwałam, a dodatkowo otrzymałam mnóstwo sugestii i pomysłów.',
     rate: '5',
     link: 'https://g.co/kgs/a9fnkvy'
   },
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/4.png',
     name: 'Wiktor Rubinski',
     opinion: 'Bardzo polecam, świetnie wykonane każde zlecenie między innymi strony dla przychodni medycznej, wypożyczalni samochodów czy sklepu z częściami samochodowymi.',
     rate: '5',
     link: 'https://g.co/kgs/bUut1HR'
   },
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/5.png',
     name: 'Małgorzata Budzalewicz',
     opinion: 'Jestem bardzo zadowolona z wykonanej usługi. Jestem laikiem w dziedzinie informatyki a dzięki Panu Danielowi mam profesjonalna stronę. Polecam wszystkim',
     rate: '5',
     link: 'https://g.co/kgs/vMNLa9X'
   },
   {
-    img: '/img/placeholder.jpg',
+    img: '/img/googleReviews/6.png',
     name: 'Magdalena Tylki',
     opinion: 'Rzetelna i uczciwa Firma. Kompetencja i kreatywność to czołowe atuty Firmy. Polecam 😊',
     rate: '5',
