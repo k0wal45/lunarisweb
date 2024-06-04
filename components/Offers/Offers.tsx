6ec2c8bfd27f428fb3abcaea0416baf0a24b1efa
k0wal45 <d.kowal.com@gmail.com>
2024-06-04 20:28:59 +0200

    fasd

--- components/Offers/Offers.tsx
+++ components/Offers/Offers.tsx
@@ -10,7 +10,7 @@ const Offers = () => {
   return (
     <section className={` bg-cover lg:bg-contain flex flex-col items-center justift-center gap-8 mx-auto p-4 my-12`}>
       <div className="flex flex-col gap-2 items-center justify-center text-center">
-        <h6 className="text-3xl lg:text-6xl font-bold">Jakie usługi oferuje?</h6>
+        <h5 className="text-3xl lg:text-5xl font-bold">Jakie usługi oferuje?</h5>
         <p className="lg:w-2/3 text-lg">Świadcze usługi tworzenia kompleksowych stron internetowycyh, logotypów i wszelkiego rodzaju materiałów reklamowych takich jak wizytówki czy banery</p>
       </div>
       
