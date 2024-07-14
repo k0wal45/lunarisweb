9a4ee36f0852e159f4ec7290502acbb9d432d7c9
k0wal45 <d.kowal.com@gmail.com>
2024-07-14 20:16:52 +0200

    sitemap and service page change further update tomorow, hope to come up with some fine design for it. no idea for now

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,84 +1,61 @@
-import { MetadataRoute } from 'next'
+import { fullOffersData } from "@/components/Offers/OffersData";
+import { GetFullPortfolioData } from "@/components/portfolio/fullData";
+import PortfolioItem from "@/components/portfolio/PortfolioPage/Item";
+import { client } from "@/sanity/client";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+const getPost = async () => {
+  const EVENTS_QUERY = `*[
+    _type == "post"
+    ] | order(publishedAt) {
+    slug,
+    publishedAt,  
+    }`;
+  const event = await client.fetch(EVENTS_QUERY);
+  return event;
+};
+
+export default async function sitemap() {
+  const events: any = await getPost();
+
+  console.log(events);
+
+  const portfolio: any = GetFullPortfolioData();
+
+  const staticUrls = [
     {
-      url: 'https://www.lunarisweb.pl',
+      url: "https://www.lunarisweb.pl",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: 'https://www.lunarisweb.pl/omnie',
+      url: "https://www.lunarisweb.pl/omnie",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/uslugi/logotypy',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/uslugi/strony-internetowe',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/uslugi/grafika-komputerowa',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/bq-cars-katowice',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "monthly",
       priority: 0.5,
     },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/merkury-medica',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/bezpieczni-w-teorii',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/oktech-autolaweta',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/oktech-klimatyzacje',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/werva-solutions',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/mali-czarodzieje',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://www.lunarisweb.pl/portfolio/oktech-autolaweta',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-  ]
+  ];
+
+  const offerUrls = fullOffersData.map((offerItem: any) => ({
+    url: "https://www.lunarisweb.pl/uslugi/" + offerItem.slug,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.9,
+  }));
+
+  const portfolioUrls = portfolio.map((portfolioItem: any) => ({
+    url: "https://www.lunarisweb.pl/portfolio/" + portfolioItem.link,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.3,
+  }));
+
+  const blogIrls = events.map((event: any) => ({
+    url: "https://www.lunarisweb.pl/blog/" + event.slug.current,
+    lastModified: event.publishedAt,
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
+
+  return [...staticUrls, ...blogIrls, ...portfolioUrls, ...offerUrls];
 }
--- components/Offers/OffersSlug/Services/Services.tsx
+++ components/Offers/OffersSlug/Services/Services.tsx
@@ -1,135 +1,99 @@
-'use client'
-import { Dispatch, SetStateAction, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useWindowSize } from "@/hooks/useWindowSize";
+"use client";
+import { motion } from "framer-motion";
+import { useState } from "react";
 import { IconType } from "react-icons";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-const Services = ({content}: any) => {
-  const [open, setOpen] = useState(content.list[0].id);
+const Services = ({ content }: { content: any }) => {
+  const [position, setPosition] = useState(0);
+
+  const shiftLeft = () => {
+    if (position > 0) {
+      setPosition((pv) => pv - 1);
+    }
+  };
+
+  const shiftRight = () => {
+    if (position < content.list.length - 1) {
+      setPosition((pv) => pv + 1);
+    }
+  };
 
   return (
-    <section className="flex flex-col gap-16 my-12 items-center justify-center p-4">
-      <h4 className="text-5xl lg:text-6xl font-bold p-4">{content.title}</h4>
-      <div className="flex flex-col lg:flex-row h-fit lg:h-[550px] w-full max-w-6xl mx-auto overflow-hidden">
-
-        {content.list.map((item: any) => {
-          return (
-            <Panel
-              key={item.id}
-              open={open}
-              setOpen={setOpen}
-              id={item.id}
-              icon={item.icon}
-              title={item.title}
-              imgSrc={item.imgSrc}
-              description={item.description}
+    <section className="overflow-hidden bg-neutral-100 px-4 py-12">
+      <div className="mx-auto max-w-6xl">
+        <div className="mb-8 flex flex-col lg:flex-row justify-between gap-4">
+          <h2 className="text-4xl font-bold leading-[1.2] md:text-5xl max-w-2xl">
+            {content.title}
+          </h2>
+          <div className="flex gap-2">
+            <button
+              className="h-fit bg-primary p-4 text-2xl text-white transition-colors hover:bg-neutral-700"
+              onClick={shiftRight}
+            >
+              <FiChevronLeft />
+            </button>
+            <button
+              className="h-fit bg-primary p-4 text-2xl text-white transition-colors hover:bg-neutral-700"
+              onClick={shiftLeft}
+            >
+              <FiChevronRight />
+            </button>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          {content.list.map((item: any, index: number) => (
+            <Feature
+              content={item}
+              key={index}
+              position={position}
+              index={index}
             />
-          );
-        })}
+          ))}
+        </div>
       </div>
     </section>
   );
 };
 
-interface PanelProps {
-  open: number;
-  setOpen: Dispatch<SetStateAction<number>>;
-  id: number;
-  icon: any;
+interface FeatureProps {
+  position: number;
+  index: number;
   title: string;
-  imgSrc: string;
   description: string;
+  Icon: IconType;
 }
 
-const Panel = ({
-  open,
-  setOpen,
-  id,
-  icon,
-  title,
-  imgSrc,
-  description,
-}: PanelProps) => {
-  const { width } = useWindowSize();
-  const isOpen = open === id;
+const Feature = ({
+  content,
+  index,
+  position,
+}: {
+  content: any;
+  index: number;
+  position: number;
+}) => {
+  const translateAmt =
+    position >= index ? index * 100 : index * 100 - 100 * (index - position);
 
   return (
-    <>
-      <button
-        className="lg:w-20 bg-white hover:bg-slate-100 transition-colors p-3 border-r-[2px] border-b-[2px] border-slate-200 flex flex-row-reverse lg:flex-col justify-between items-center gap-4 relative group rounded-xl"
-        onClick={() => setOpen(id)}
-      >
-        <div className="hidden lg:block"></div>
-        <p className="text-xl whitespace-nowrap lg:rotate-[-90deg] lg:tracking-widest">
-          {title}
-        </p>
-        <div className="w-6 lg:w-full aspect-square bg-indigo-600 text-white grid place-items-center lg:text-2xl">
-          {icon}
-        </div>
-      </button>
-
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            key={`panel-${id}`}
-            variants={width && width > 1024 ? panelVariants : panelVariantsSm}
-            initial="closed"
-            animate="open"
-            exit="closed"
-            style={{
-              backgroundImage: `url(${imgSrc})`,
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-            }}
-            className="w-full h-full overflow-hidden relative  flex items-end rounded-xl"
-          >
-            <motion.div
-              variants={descriptionVariants}
-              initial="closed"
-              animate="open"
-              exit="closed"
-              className="px-4 py-2 bg-black/40 backdrop-blur-sm text-white lg:text-2xl"
-            >
-              <p>{description}</p>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
+    <motion.div
+      animate={{ x: `${-translateAmt}%` }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.35,
+      }}
+      className={`relative flex min-h-[250px] w-10/12 max-w-lg shrink-0 flex-col justify-between overflow-hidden p-8 shadow-lg md:w-3/5 ${
+        index % 2 ? "bg-primary text-white" : " bg-white"
+      }`}
+    >
+      <div className="absolute right-2 top-2 text-7xl opacity-20">
+        {content.icon}
+      </div>
+      <h3 className="mb-8 text-3xl font-bold">{content.title}</h3>
+      <p>{content.description}</p>
+    </motion.div>
   );
 };
 
 export default Services;
-
-const panelVariants = {
-  open: {
-    width: "100%",
-    height: "100%",
-  },
-  closed: {
-    width: "0%",
-    height: "100%",
-  },
-};
-
-const panelVariantsSm = {
-  open: {
-    width: "100%",
-    height: "200px",
-  },
-  closed: {
-    width: "100%",
-    height: "0px",
-  },
-};
-
-const descriptionVariants = {
-  open: {
-    opacity: 1,
-    y: "0%",
-    transition: {
-      delay: 0.125,
-    },
-  },
-  closed: { opacity: 0, y: "100%" },
-};
