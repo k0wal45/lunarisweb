f6945c2e58c8cb4240e31ebc9f376b554b58ea11
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2025-04-18 17:07:11 +0200

    Update layout.tsx

--- app/(mainPage)/layout.tsx
+++ app/(mainPage)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from 'next/script';
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
@@ -40,6 +41,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
+      <head>
+        <Script
+          id="local-business-jsonld"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessData) }}
+        />
+      </head>
       <body className={`${montserrat.className} bg-white text-black`}>
         <Navbar />
         {children}
@@ -48,4 +56,36 @@ export default function RootLayout({
       <GoogleAnalytics gaId={"G-LC4SHK59XE"} />
     </html>
   );
+}
+
+const localBusinessData = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Lunaris Web",
+  "image": "https://www.lunarisweb.pl/_next/image?url=%2Fimg%2Flogo.png&w=96&q=75",
+  "description": "Tworzenie nowoczesnych stron internetowych i stron firmowych w Katowicach.",
+  "url": "https://lunarisweb.pl",
+  "telephone": "+48-664-447-556",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "ul. Szczecińska 20a",
+    "addressLocality": "Katowice",
+    "addressRegion": "śląskie",
+    "postalCode": "40-139",
+    "addressCountry": "PL"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 50.278630966331875,
+    "longitude": 19.011518087354396
+  },
+  "openingHours": [
+    "Mo-Fr 09:00-17:00",
+    "Sa-Su 10:00-14:00"
+  ],
+  "sameAs": [
+    "https://www.facebook.com/LunarisWeb",
+    "https://www.instagram.com/lunarisweb.pl/"
+  ],
+  "priceRange": "PLN 1000–10000"
 }
